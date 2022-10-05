3e05e154fb0cbaa22a822898121bbebeb07ec98e
Caleb <calebmutuku4@gmail.com>
2022-10-05 14:39:33 +0300

    added the urls and view for the pages

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls.static import static
 urlpatterns = [   
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
-
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('my-account/',views.myaccount,name='my-account'),
+    path('wishlist/',views.wishlist,name='wishlist'),
 
 ]    
 
--- myapp/views.py
+++ myapp/views.py
@@ -7,3 +7,27 @@ def index(request):
 
 def about(request):
     return render (request, "about.html")
+
+def cart(request):
+    return render (request, "cart.html")
+
+def checkout(request):
+    return render (request, "checkout.html")
+
+def myaccount(request):
+    return render (request, "my-account.html")
+
+def wishlist(request):
+    return render (request, "wishlist.html")
+
+def seadlings(request):
+    return render (request, "seadlings.html")
+
+def indigenous_seeds(request):
+    return render (request, "indigenous_seeds.html") 
+
+def books(request):
+    return render (request, "books.html")
+
+def other_products(request):
+    return render (request, "other_products.html")
