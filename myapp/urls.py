59cabc1efdf3416cdb82565829e11a7aeaf63a8a
Caleb <calebmutuku4@gmail.com>
2022-10-05 12:43:18 +0300

    worked on the about template

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [   
     path('',views.index,name='index'),
+    path('about/',views.about,name='about'),
+
 
 ]    
 
